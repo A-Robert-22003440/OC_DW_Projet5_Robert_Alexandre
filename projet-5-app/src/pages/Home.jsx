dff205c4e0822d3e13d465819ce88288eb08fba0
Alexandre Robert <alexandre.robert.22003440@gmail.com>
2026-03-07 09:05:32 +0100

    ajout des prinpal branche

---
+++ projet-5-app/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <section className="page">
+      <h2>Accueil</h2>
+      <p>Le routage React Router est fonctionnel.</p>
+    </section>
+  )
+}
+
+export default Home
---
+++ projet-5-app/src/router/AppRouter.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import MainLayout from '../layout/MainLayout'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Logements from '../pages/Logements'
+import NotFound from '../pages/NotFound'
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="logements" element={<Logements />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
